14243dff89cd20efeada0163f1ce876719e8f481
Bruno de Araújo Alves <me@baraus.dev>
2023-10-04 00:13:47 -0300

    fix: change footer message
    Signed-off-by: Bruno de Araújo Alves <me@baraus.dev>

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,19 +3,19 @@ export default function Footer() {
     <footer className="py-6 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Built by{" "}
+          Construído por{" "}
           <a
             className="font-medium underline underline-offset-4"
-            href="https://twitter.com/shadcn"
+            href="https://baraus.dev"
             rel="noreferrer"
             target="_blank"
           >
-            shadcn
+            baraus.dev
           </a>
-          . The source code is available on{" "}
+          . Código fonte disponível no{" "}
           <a
             className="font-medium underline underline-offset-4"
-            href="https://github.com/shadcn-ui/ui"
+            href="https://github.com/devbaraus/kingsystem-client"
             rel="noreferrer"
             target="_blank"
           >
